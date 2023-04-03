5458f0bdb2111315a665f3f0afa36a22b8f6a677
kubra demirkaya <kubrademirkaya3@gmail.com>
2023-04-04 02:11:41 +0300

    location operations and design fixes

--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -12,23 +12,60 @@
 
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
     @IBOutlet weak var windSpeedLabel: UILabel!
     @IBOutlet weak var feelsLikeLabel: UILabel!
     @IBOutlet weak var currentTempLabel: UILabel!
     let api = "b93e4d0e8683b3a1a6bb64045463171e"
     
+    //Koordinat işlemleri için gerekli değişken tanımlamaları
+    var currentLatitude = 0.0
+    var currentLongitude = 0.0
+    var latitude : Double?
+    var longitude : Double?
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         title = "WEATHER"
+        
+        getUserLocation()
+        getData()
+        
     }
-
-    @IBAction func getDataButtonClicked(_ sender: Any) {
+    
+    //Geçerli anlık konumu alma işlemleri getUserLocation() fonksiyonu içinde gerçekleştiriliyor
+    func getUserLocation() {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        //Alınan geçerli konum değişkene atanıyor
+        currentLatitude = locationManager.location?.coordinate.latitude ?? currentLatitude
+        currentLongitude = locationManager.location?.coordinate.longitude ?? currentLongitude
+        
+        locationManager.stopUpdatingLocation()
+    }
+    
+    //Konum için gerekli olan CoreLocation sınıfının LocationManager fonksiyonu delegate ediliyor
+    //Geçerli anlık konum alınıyor
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location: CLLocationCoordinate2D = manager.location?.coordinate else { return }
+        latitude = location.latitude
+        longitude = location.longitude
+    }
+    
+    
+    func getData() {
         //1. Web adresine istek gönderiliyor
-        let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?lat=40.773172&lon=30.3626394&appid=\(api)")
+        //url'de koordinatlar, geçerli koordinatların atandığı değişkenler olarak belirleniyor
+        let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?lat=\(currentLatitude)&lon=\(currentLongitude)&appid=\(api)")
         let session = URLSession.shared
         let task = session.dataTask(with: url!) { data, response, error in
             if error != nil {
@@ -55,7 +92,18 @@ class ViewController: UIViewController {
                             if let wind = jsonResponse!["wind"] as? [String:Any] {
                                 if let speed = wind["speed"] as? Double {
                                     self.windSpeedLabel.text = String(Int(speed))
+                                    print(String(Int(speed)))
                                 }
+                            }
+                            if let weatherArray = jsonResponse!["weather"] as? [[String:Any]] {
+                                if let weather = weatherArray[0] as? [String:Any] {
+                                    if let description = weather["description"] as? String {
+                                        self.descriptionLabel.text = description
+                                    }
+                                }
+                            }
+                            if let name = jsonResponse!["name"] as? String {
+                                self.locationLabel.text = name
                             }
                         }
                         
@@ -69,7 +117,5 @@ class ViewController: UIViewController {
         //Session işleminin gerçekleşip, çalıştırılması için;
         task.resume()
     }
-    
-    
 }
 
